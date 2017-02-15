802907347dc613dad3503fd21d5303019f216e47
Rodrigo Adones Vicencio <RodigoAdones@MacBook-Pro-de-Rodrigo.local>
2017-02-14 22:53:16 -0300

    auth with google. config firebase as backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user : null
+    }
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged( user => {
+      this.setState({
+        user
+      });
+    });
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} se ha logeado`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} se ha desconectado`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  renderLoginButton () {
+    if (this.state.user) {
+      return (
+        <div>
+          <img width='100' src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      )
+    } else {
+      return <button onClick={this.handleAuth}>Logear con Google</button>
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>KrasGram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.renderLoginButton()}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase'
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA4tpG_mP8fmPc5ImS1jfpjJm6lBlCQcEw",
+    authDomain: "krasgram.firebaseapp.com",
+    databaseURL: "https://krasgram.firebaseio.com",
+    storageBucket: "krasgram.appspot.com",
+    messagingSenderId: "402613122942"
+  });
 
 ReactDOM.render(
   <App />,
