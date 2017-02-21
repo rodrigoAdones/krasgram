f834a417492eafd6abede117d51b1061f6080147
Rodrigo Adones Vicencio <RodigoAdones@MacBook-Pro-de-Rodrigo.local>
2017-02-21 06:17:37 -0300

    save in firebase database and load new contributions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
+import React, { Component } from 'react'
+import firebase from 'firebase'
 import FileUpload from './FileUpload'
 
-import './App.css';
+import './App.css'
 
 class App extends Component {
   constructor () {
-    super();
+    super()
     this.state = {
-      user : null
+      user: null,
+      pictures: []
     }
 
-    this.handleAuth = this.handleAuth.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
+    this.handleAuth = this.handleAuth.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+    this.handleUpload = this.handleUpload.bind(this)
   }
 
   componentWillMount () {
-    firebase.auth().onAuthStateChanged( user => {
+    firebase.auth().onAuthStateChanged(user => {
       this.setState({
         user
-      });
-    });
+      })
+    })
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      })
+    })
   }
 
   handleAuth () {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new firebase.auth.GoogleAuthProvider()
 
     firebase.auth().signInWithPopup(provider)
     .then(result => console.log(`${result.user.email} se ha logeado`))
@@ -37,14 +45,56 @@ class App extends Component {
     .catch(error => console.log(`Error ${error.code}: ${error.message}`))
   }
 
+  handleUpload (event) {
+    const file = event.target.files[0]
+    const strorageRef = firebase.storage().ref(`/fotos/${file.name}`)
+    const task = strorageRef.put(file)
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      this.setState({
+        uploadValue: percentage
+      })
+    }, error => {
+      console.log(error.message)
+    }, () => {
+      /* this.setState({
+        uploadValue: 100,
+        picture: task.snapshot.downloadURL
+      }) */
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      }
+
+      const dbRef = firebase.database().ref('pictures')
+
+      const newPicture = dbRef.push()
+
+      newPicture.set(record)
+    })
+  }
+
   renderLoginButton () {
     if (this.state.user) {
       return (
         <div>
-          <img width='100' src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <img width='100' src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}!</p>
           <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload />
+          <FileUpload onUpload={this.handleUpload} />
+          {
+            this.state.pictures.map(picture => (
+              <div>
+                <img width='100%' src={picture.image} alt='upload_image' />
+                <br />
+                <img width='48' src={picture.photoURL} alt={picture.displayName} />
+                <br />
+                <span>{picture.displayName}</span>
+              </div>
+            )).reverse()
+          }
         </div>
       )
     } else {
@@ -52,18 +102,18 @@ class App extends Component {
     }
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className='App'>
+        <div className='App-header'>
           <h2>KrasGram</h2>
         </div>
-        <p className="App-intro">
+        <p className='App-intro'>
           {this.renderLoginButton()}
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,47 +1,22 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
+import React, { Component } from 'react'
 
 class FileUpload extends Component {
   constructor () {
-    super();
+    super()
     this.state = {
-      uploadValue: 0,
-      picture: null
+      uploadValue: 0
     }
-    this.handleUpload = this.handleUpload.bind(this)
-  }
-
-  handleUpload (event) {
-    const file = event.target.files[0];
-    const strorageRef = firebase.storage().ref(`/fotos/${file.name}`);
-    const task = strorageRef.put(file);
-
-    task.on('state_changed', snapshot => {
-      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      this.setState({
-        uploadValue: percentage
-      })
-    }, error => {
-      console.log(error.message)
-    }, () => {
-      this.setState({
-        uploadValue: 100,
-        picture: task.snapshot.downloadURL
-      })
-    });
   }
 
   render () {
     return (
       <div>
-        <progress value={this.state.uploadValue} max="100"></progress>
-        <br/>
-        <input type='file' onChange={this.handleUpload}/>
-        <br/>
-        <img width='320' src={this.state.picture} alt=''/>
+        <progress value={this.state.uploadValue} max='100' />
+        <br />
+        <input type='file' onChange={this.props.onUpload} />
       </div>
     )
   }
 }
 
-export default FileUpload;
+export default FileUpload
